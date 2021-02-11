cc38609bb1088318d31cdb387506ee2c50780641
Mohsen Mollanoori <mohsen.mollanoori@clio.com>
2021-02-11 15:46:00 -0700

    Finish parsing expressions

--- src/SrcParser.hs
+++ src/SrcParser.hs
@@ -10,7 +10,7 @@ import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void String
 
-keywords = ["def", "do", "end"]
+keywords = ["def", "do", "end", "if", "then", "else", "let"]
 
 sc :: Parser ()
 sc = L.space
@@ -38,29 +38,55 @@ lowerCaseName label = do
     else
       return name
 
+upperCaseName :: String -> Parser String
+upperCaseName label = do
+    first <- upperChar <?> label
+    rest <- many alphaNumChar
+    let name = first:rest
+    if name `elem` keywords
+    then
+      fail "Keyword found"
+    else
+      return name
+
 functionParser :: Parser SrcFunctionDef 
 functionParser = do
   keyword "def"
   name <- lowerCaseName "functionName"
   args <- between (symbol "(") (symbol ")") (lowerCaseName "functionArg" `sepBy` symbol ",")
   expressions <- between (keyword "do") (keyword "end") (some $ try expressionParser)
-  -- ...
   return $ SrcFunctionDef name args expressions
 
 expressionParser :: Parser SrcExp
-expressionParser = choice [primitiveParser, listParser, varParser] <?> "expression"
+expressionParser = choice [letParser, ifParser, primitiveParser, listParser, try functionCallParser, varParser] <?> "expression"
   where
       listParser = do
           items <- between (symbol "[") (symbol "]") (expressionParser `sepBy` symbol ",")
           return $ SrcExpList items
-
-      primitiveParser = do
-          val <- valueParser
-          return $ SrcExpPrimitive val
-
-      varParser = do
+      primitiveParser = fmap SrcExpPrimitive valueParser
+      varParser = fmap SrcExpVar (lowerCaseName "variableName")
+      letParser = do
+          keyword "let"
           name <- lowerCaseName "variableName"
-          return $ SrcExpVar name
+          space
+          keyword "="
+          value <- expressionParser
+          return $ SrcLetExp name value
+      ifParser = do
+          keyword "if"
+          condition <- expressionParser
+          keyword "then"
+          thenBlock <- some $ try expressionParser
+          keyword "else"
+          elseBlock <- some $ try expressionParser
+          keyword "end"
+          return $ SrcIfExp condition thenBlock elseBlock
+      functionCallParser = do
+          moduleName <- optional (upperCaseName "ModuleName" <* symbol ".")
+          functionName <- lowerCaseName "functionName"
+          params <- between (symbol "(") (symbol ")") (expressionParser `sepBy` symbol ",")
+          return $ SrcFunctionCall moduleName functionName params
+
 
 valueParser :: Parser SrcPrimitive
 valueParser = choice [holeParser, intParser]
