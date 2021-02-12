c1899982026d7ac395de84f8a2ec071286876a2d
Mohsen Mollanoori <mohsen.mollanoori@clio.com>
2021-02-11 19:40:46 -0700

    Native functions implemented

--- src/TranspileJS.hs
+++ src/TranspileJS.hs
@@ -35,20 +35,35 @@ transpileExp (SrcIfExp cond thenBlock elseBlock) =
                      , "}"
                      , "})()"
                      ]
-transpileExp (SrcFunctionCall (Just moduleName) funcName args) =
-    case (moduleName, funcName) of
-        ("IO", "print") -> concat ["console.log(", transpileExp (head args), ")"]
-        ("List", "append") -> concat ["(", transpileExp (head args), ").concat(", transpileExp (last args), ")"]
-        ("List", "filter") -> concat ["(", transpileExp (head args), ").filter(", transpileExp (last args), ")"]
-        ("List", "get") -> concat ["(", transpileExp (head args), ")[", transpileExp (last args), "]"]
-        ("List", "length") -> concat ["(", transpileExp (head args), ").length"]
-        ("List", "sublist") -> concat ["(", transpileExp (head args), ").slice(", transpileExp (args !! 1), ", ", transpileExp (args !! 2), ")"]
-        ("Int", "lte") -> opstr "<="
-        ("Int", "lt") -> opstr "<"
-        ("Int", "gte") -> opstr ">="
-        ("Int", "sub") -> opstr "-"
-        (_, _) -> error $ "Unknown native function: " ++ moduleName ++ "." ++ funcName
+transpileExp (SrcFunctionCall (Just moduleName) funcName realArgs) =
+    if holeCount > 0
+    then
+        concat [ "function(", intercalate ", " ["hole_" ++ show i | i <- [1..holeCount]], ") { return (", funcCall ,"); }" ]
+    else
+        funcCall
     where
+        holeCount = length $ filter (SrcExpPrimitive SrcHole ==) realArgs
+        
+        replaceHoles [] _ = []
+        replaceHoles (SrcExpPrimitive SrcHole:args) (i:is) = SrcExpVar ("hole_" ++ show i):replaceHoles args is
+        replaceHoles (x:args) is = x:replaceHoles args is
+
+        args = replaceHoles realArgs [1..holeCount]
+
+        funcCall =
+            case (moduleName, funcName) of
+                ("IO", "print") -> concat ["console.log(", transpileExp (head args), ")"]
+                ("List", "append") -> concat ["(", transpileExp (head args), ").concat(", transpileExp (last args), ")"]
+                ("List", "filter") -> concat ["(", transpileExp (head args), ").filter(", transpileExp (last args), ")"]
+                ("List", "get") -> concat ["(", transpileExp (head args), ")[", transpileExp (last args), "]"]
+                ("List", "length") -> concat ["(", transpileExp (head args), ").length"]
+                ("List", "sublist") -> concat ["(", transpileExp (head args), ").slice(", transpileExp (args !! 1), ", ", transpileExp (args !! 2), ")"]
+                ("Int", "lte") -> opstr "<="
+                ("Int", "lt") -> opstr "<"
+                ("Int", "gte") -> opstr ">="
+                ("Int", "sub") -> opstr "-"
+                (_, _) -> error $ "Unknown native function: " ++ moduleName ++ "." ++ funcName
+
         opstr op  = concat [ "(", transpileExp (head args), ") ", op, " (", transpileExp (last args), ")"]
 
 transpileExp (SrcFunctionCall Nothing funcName args) = concat [funcName, "(", intercalate ", " $ fmap transpileExp args ,")"]
